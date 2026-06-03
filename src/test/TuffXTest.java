7a8a8604b267ce8c012c76082f4e0083bcf8e096
TrainBoy15 <162745871+Trainboy15@users.noreply.github.com>
2026-06-03 10:37:09 -0400

    Remove old tests

--- src/test/TuffXTest.java
+++ src/test/TuffXTest.java
@@ -28,39 +28,10 @@ class TuffXTest {
         assertTrue(plugin.isEnabled(), "Plugin should be enabled after load");
     }
 
-    @Test
-    void latestVersionIsNullOnStartup() {
-        assertNull(plugin.latestAvailableVersion,
-            "No update should be detected synchronously at startup");
-    }
 
     @Test
     void reloadDoesNotThrow() {
         assertDoesNotThrow(() -> plugin.reloadTuffX(),
             "reloadTuffX() should not throw");
-    }
-
-    @Test
-    void opReceivesUpdateMessageWhenUpdateAvailable() {
-        plugin.latestAvailableVersion = "2.0.0";
-
-        PlayerMock player = server.addPlayer();
-        player.setOp(true);
-        player.assertNoMoreSaid();
-
-        player.disconnect();
-        server.addPlayer(player.getName());
-
-        player.assertSaid("§e[TuffX] §fA new version is available: §a2.0.0 §f(running §c1.0.0-patch§f)");
-    }
-
-    @Test
-    void nonOpDoesNotReceiveUpdateMessage() {
-        plugin.latestAvailableVersion = "2.0.0";
-
-        PlayerMock player = server.addPlayer();
-        player.setOp(false);
-
-        player.assertNoMoreSaid();
     }
 }
